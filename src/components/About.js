53aecfcb4d0cd53b98c99512fd76b6afe671855d
Logan Miller <loganmiller214@gmail.com>
2022-04-27 15:54:32 -0400

    commit tree 8574beea562d7e795262ffecd47e42f7e54ceb01

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Obj from "../data/user";
 
-function About() {
+function About(Obj) {
   return (
     <div id="about">
       <h2>About Me</h2>
       <p>I Made This</p>
-      <img src={image} alt="I made this" />
+      <img src={Obj.image} alt="I made this" />
     </div>
   );
 }
+
+export default About
